dd701e2299e4f7941c4abde5d9d9f8234701ea9e
AbuBaker-AB <145045004+AbuBaker-AB@users.noreply.github.com>
2026-03-04 22:56:00 +0600

    Add sample print statements in main
    Replace empty main with several print statements that output example "box" messages. Adds multiple string prints (two repeated box sequences) to module_4/bin/class_01.dart to demonstrate basic use of print in the entrypoint.

--- module_4/bin/class_01.dart
+++ module_4/bin/class_01.dart
@@ -1,3 +1,11 @@
-void main(){
-  
+void main() {
+  //Functions
+  print("Here is a box - Give");
+  print('Take this box');
+  print("Label it");
+  print('Store it');
+  print("Here is box 2 - Give");
+  print('Take this box');
+  print("Label it");
+  print('Store it');
 }
