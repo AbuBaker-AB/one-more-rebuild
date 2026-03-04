245af02abfae6813788cb7818c9c9f165e94a9f8
AbuBaker-AB <145045004+AbuBaker-AB@users.noreply.github.com>
2026-03-04 23:09:02 +0600

    Add comment about knowing what you don't know
    Insert a non-functional reminder comment in module_4/bin/class_01.dart emphasizing that "knowing what you don't know is extremely important." This is a documentation-only change and does not alter program behavior.

--- module_4/bin/class_01.dart
+++ module_4/bin/class_01.dart
@@ -10,6 +10,8 @@ void main() {
 // DRY - Don't Repeat Yourself
 //S.O.L.I.D - Single Responsibility Principle
 
+//NOTE: Knowing what you don't know is extremely important.
+
 void labelAndStore() {
   print("Take this box");
   print("Label it");
