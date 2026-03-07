20f070e7bec3c27acd5b40fb487d02d00bcddb5e
AbuBaker-AB <145045004+AbuBaker-AB@users.noreply.github.com>
2026-03-07 22:56:06 +0600

    Add examples for positional parameters
    Add greetings and showUserDetails functions to module_4/bin/class_02.dart and a main() that demonstrates using positional parameters. Includes comments explaining positional-parameter rules and prints sample output for name, age, and nationality.

--- module_4/bin/class_02.dart
+++ module_4/bin/class_02.dart
@@ -1,1 +1,22 @@
 //Parameters
+
+// void main() {
+//   greetings("Alice");
+// }
+
+void greetings(String name) {
+  print('Hello, $name!');
+}
+
+//01. Positional parameters
+// • Must maintain the order
+// • Must be same number of arguments as parameters
+
+void main() {
+  greetings('John');
+  showUserDetails('John', 30,'American');
+}
+
+void showUserDetails(String name, int age, String nationality) {
+  print('Name: $name, Age: $age, Nationality: $nationality');
+}
