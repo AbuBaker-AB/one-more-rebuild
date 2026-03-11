ad3e7eeff8aea24e82478f634a7c876bf2d90625
AbuBaker-AB <145045004+AbuBaker-AB@users.noreply.github.com>
2026-03-11 23:00:38 +0600

    Add House class and sample objects
    Introduce a House class with a constructor and three final properties (name, address, color). Add a placeholder <Class_Name> scaffold for future classes. Update main() to create two House instances as examples of using the class.

--- module_5/bin/Class_1.dart
+++ module_5/bin/Class_1.dart
@@ -1,1 +1,25 @@
 //classes and objects
+
+class House {
+  //This is constructor of the class House
+  House(this.name, this.address, this.color);
+
+  final String name;
+  final String address;
+  final String color;
+}
+
+class <Class_Name> {
+  //COnstrutor
+
+  //Properties
+  //Methods
+}
+
+void main() {
+  //Object
+  final myHouse = House('My House', 'Gulshan-1, Dhaka', 'Blue');
+
+  final anotherHouse = House('Another House', 'Gulshan-1, Dhaka', 'Red');
+
+}
